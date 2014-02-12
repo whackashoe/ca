26d99b91275fd5aece70e81ff8e2c0bc6d9590cc
Jett <whackashoe@gmail.com>
2014-02-12 01:05:46 -0600

    add bad doc

--- test/treet/traversalhelper.hpp
+++ test/treet/traversalhelper.hpp
@@ -4,10 +4,16 @@
 #include <array>
 #include "orientation.hpp"
 
+/****************************************
+ *
+ *TRAVERSALHELPER :: MOVE DIRECTION / CONTINUE?
+ * 
+ ****************************************/
+
 template <uint Dimension>
 struct TraversalHelper {
-	std::array<orientation, Dimension> way;
-	bool deeper;
+	std::array<orientation, Dimension> way; //the direction of our traversal
+	bool deeper; //should be set to true if we wish to go further up/down. if not, set to false. 
 };
 
 #endif
